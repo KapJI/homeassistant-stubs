86ad56bf4429d604319c5e0f463913dab15f476a
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2021-03-27 16:35:00 +0000

    Implement stubs update

--- update_stubs.py
+++ update_stubs.py
@@ -1,28 +1,61 @@
 #!/usr/bin/env python3
+"""Fetch the latest homeassistant tag and generate typing stubs for it."""
 
 import os
+import subprocess
 import sys
 from pathlib import Path
 from typing import List
 
-def get_typed_folder(homeassistant_root: Path) -> List[Path]:
-    """Get list of strictly typed folders from Home Assistant config"""
-    setup_cfg = homeassistant_root / "setup.cfg"
-    matched_lines = []
-    with setup_cfg.open() as f:
-        matched_lines = [line for line in f.readlines() if line.startswith("[mypy-")]
-    if matched_lines:
-        raise Exception("can't find mypy config in setup.cfg")
-    if len(matched_lines) > 1:
-        raise Exception("too many mypy config entries in setup.cfg, update the script")
 
 def main() -> int:
     """Main function"""
     repo_root = Path(os.path.dirname(os.path.realpath(__file__)))
     homeassistant_root = repo_root / "homeassistant_core"
-    typed_folders = get_typed_folder(homeassistant_root)
-
+    typed_paths = get_typed_paths(homeassistant_root)
+    generate_stubs(typed_paths, repo_root / "homeassistant_stubs")
     return 0
+
+
+def get_typed_paths(homeassistant_root: Path) -> List[Path]:
+    """Get list of strictly typed paths from Home Assistant config"""
+    setup_cfg = homeassistant_root / "setup.cfg"
+    matched_lines: List[str] = []
+    with setup_cfg.open() as fp:
+        matched_lines = [line for line in fp.readlines() if line.startswith("[mypy-")]
+    if not matched_lines:
+        raise Exception("can't find mypy config in setup.cfg")
+    if len(matched_lines) > 1:
+        raise Exception("too many mypy entries in setup.cfg, update the script")
+    mypy_config = matched_lines[0][len("[mypy-") : -len("]\n")]
+    typed_paths: List[Path] = []
+    for item in mypy_config.split(","):
+        if item.endswith(".*"):
+            typed_path = homeassistant_root / Path(
+                item[: -len(".*")].replace(".", os.path.sep)
+            )
+            assert typed_path.is_dir(), typed_path
+        else:
+            item = item.replace(".", os.path.sep)
+            typed_path = homeassistant_root / Path(item + ".py")
+            if not typed_path.is_file():
+                continue
+        typed_paths.append(typed_path)
+    return typed_paths
+
+
+def generate_stubs(typed_paths: List[Path], output_folder: Path) -> None:
+    """Use stubgen to generate typing stubs for all typed paths"""
+    print("Generating stubs...")
+    command_args: List[str] = [
+        "poetry",
+        "run",
+        "stubgen",
+        "-o",
+        str(output_folder),
+    ] + [str(folder) for folder in typed_paths]
+    subprocess.run(command_args, check=True)
+
 
 if __name__ == "__main__":
     sys.exit(main())
